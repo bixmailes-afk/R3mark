98a62686f039186cce46c77831ee01f026351b4f
bixmailes-afk <bixmail.es@gmail.com>
2026-06-17 22:03:33 +0200

    Changed prefixes 1

--- public/petezah/config.js
+++ public/petezah/config.js
@@ -1,13 +1,13 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/petezah/petezah/",
+  prefix: "/R3mark/R3mark/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/petezah/handler.js",
-  client: "/petezah/client.js",
-  bundle: "/petezah/bundle.js",
-  config: "/petezah/config.js",
-  sw: "/petezah/rizz.sw.js",
+  handler: "/R3mark/handler.js",
+  client: "/R3mark/client.js",
+  bundle: "/R3mark/bundle.js",
+  config: "/R3mark/config.js",
+  sw: "/R3mark/rizz.sw.js",
 };
